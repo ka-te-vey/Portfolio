c3a99f1fcb3de8c263bb52a90c7445a88d271eb8
ka-te-vey <oscar19092019@gmail.com>
2026-06-25 16:21:43 +0700

    add landing page

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,12 @@
 import Preloading from './pages/Preloading';
 import './App.css';
+import About from './pages/About.jsx';
 
 const App = () => {
   return (
     <div>
-      <Preloading />
+      <Preloading />  
+      <About />
     </div>
   )
 }
